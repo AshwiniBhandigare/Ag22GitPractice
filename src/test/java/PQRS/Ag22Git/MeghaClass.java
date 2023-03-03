75bcf1b88af2589421b0a2713f2bb60f30094b55
Meeghaa <meghagangavane69@gmail.com>
2023-03-03 23:07:57 +0530

    Added Meghas File

---
+++ src/test/java/PQRS/Ag22Git/MeghaClass.java
@@ -0,0 +1,10 @@
+package PQRS.Ag22Git;
+
+public class MeghaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first Pro");
+	}
+
+}
